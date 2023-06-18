4ebeaac3fb047a03842954d209046b6ff733403a
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-06-18 17:38:16 -0300

    initial commit, first components and class

---
+++ site-nar-idealize/src/components/EmailButton.js
@@ -0,0 +1,9 @@
+function EmailButton() {
+    return (
+        <div>
+            {/* criar botao que encaminha pra um email  */}
+        </div>
+    )
+}
+
+export default EmailButton
---
+++ site-nar-idealize/src/components/PageHeader.js
@@ -0,0 +1,12 @@
+import EmailButton from "./EmailButton"
+
+function PageHeader() {
+    return (
+        <div>
+            <h1>NAR Idealize</h1>
+            <EmailButton/>
+        </div>
+    )
+}
+
+export default PageHeader
