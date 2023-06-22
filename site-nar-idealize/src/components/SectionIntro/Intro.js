b0e4d0b26bf1a63677de69e60f6af6634e5e16a0
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-06-22 20:40:26 -0300

    header e intro perfeitos

--- site-nar-idealize/src/components/SectionIntro/Intro.js
+++ site-nar-idealize/src/components/SectionIntro/Intro.js
@@ -6,7 +6,7 @@ function Intro() {
     return (
         <div className={styles.textContainer}>
             <h1>Soluções paisagísticas ideais <br/>pra você, para seu evento <br/>e ambientes corporativo.</h1>
-            <p>NAR Idealize oferece soluções de paisagismo/decoração e projetos <br/>de qualidade para transformar os ambientes internos ou externos. Design e criação de<br/>ambientes, nós nos esforçamos para criar um cenário com naturalidade para você.</p>
+            <p>NAR Idealize oferece soluções paisagísticas, decoração e projetos <br/>de qualidade para transformar ambientes internos ou externos. <br/>Nós nos esforçamos para criar um cenário com naturalidade para você.</p>
         </div>
     );
 }
