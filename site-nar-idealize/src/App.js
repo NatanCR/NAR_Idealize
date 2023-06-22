6a70a1b230a4710ef203283a51061739e5d7b05f
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-06-22 15:32:58 -0300

    começando a section intro

--- site-nar-idealize/src/App.js
+++ site-nar-idealize/src/App.js
@@ -1,11 +1,12 @@
 // import './App.css';
 import PageHeader from './components/SectionHeader/PageHeader';
-// import Intro from './components/SectionIntro/Intro';
+import Intro from './components/SectionIntro/Intro';
 
 function App() {
   return (
     <div className="App">
       <PageHeader/>
+      <Intro/>
     </div>
   );
 }
--- site-nar-idealize/src/components/SectionIntro/Intro.js
+++ site-nar-idealize/src/components/SectionIntro/Intro.js
@@ -1,9 +1,15 @@
-import styles from './Intro.module.css' 
+import React from 'react';
+import styles from './Intro.module.css';
+import backgroundImage from '..images/Parede-verde.jpeg';
 
 function Intro() {
+    const containerStyle = {
+        backgroundImage: 'url(${backgroundImage})',
+    };
+
     return (
-        <div className={styles.textContainer}>
-            {/* uma imagem de background e um texto por cima  */}
+        <div className={styles.textContainer} sytles={containerStyle}>
+            <h1>Texto sobreposto</h1>
         </div>
     )
 }
