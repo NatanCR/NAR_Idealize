9649e60ddc076fcaf961f1f4399f2d4b60864bc6
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-07-22 15:27:16 -0300

    feat: iniciando section "sobre"

--- site-nar-idealize/src/App.js
+++ site-nar-idealize/src/App.js
@@ -3,6 +3,7 @@ import PageHeader from './components/SectionHeader/PageHeader';
 import Intro from './components/SectionIntro/Intro';
 import CarouselBS from './components/SectionCarousel/CarouselBS';
 import ServicesGrid from './components/SectionGrid/ServicesGrid';
+import About from './components/SectionAbout/About';
 
 function App() {
   return (
