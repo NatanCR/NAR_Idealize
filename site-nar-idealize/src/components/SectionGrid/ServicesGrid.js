50d27a95b88bfdb5568e64c975580ac1a01d6135
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-07-29 20:51:25 -0300

    melhorando textos

--- site-nar-idealize/src/components/SectionGrid/ServicesGrid.js
+++ site-nar-idealize/src/components/SectionGrid/ServicesGrid.js
@@ -11,13 +11,13 @@ function ServicesGrid(){
                 <h1>Serviços</h1>
                 <div className="row justify-content-center">
                     <div class="col-md">
-                    <ImageGrid img={image3} title="Parede Instagramável" text="Transformamos sua parede."/>
+                    <ImageGrid img={image3} title="Parede Verde Natural" text="É importante medir cuidadosamente a área verde disponível para definir o projeto paisagístico ideal."/>
                     </div>
                     <div class="col-md">
-                    <ImageGrid img={image5} title="Jardim suspenso" text="Deixamos o ambiente do seu evento mais verde e sustentável."/>
+                    <ImageGrid img={image5} title="Jardim Suspenso" text="É preciso selecionar as espécies de plantas que valorizem o espaço e de sentido a proposta do projeto."/>
                     </div>
                     <div class="col-md">
-                    <ImageGrid img={image15} title="Vasos decorativos" text="Montamos os vasos ideais que transformam seu evento."/>
+                    <ImageGrid img={image15} title="Vasos Decorativos" text="É importante desenhar todos os elementos de forma detalhada, para que o projeto possa ser concretizado com sucesso."/>
                     </div>
                 </div>
             </div>
--- site-nar-idealize/src/components/SectionIntro/Intro.js
+++ site-nar-idealize/src/components/SectionIntro/Intro.js
@@ -5,7 +5,7 @@ function Intro() {
 
     return (
         <div className={styles.textContainer}>
-            <h1>Soluções paisagísticas ideais <br/>pra você, para seu evento <br/>e ambientes corporativo.</h1>
+            <h1>Soluções paisagísticas ideais <br/>pra você, para seu evento <br/>e ambientes corporativos.</h1>
             <p>NAR Idealize oferece soluções paisagísticas, decoração e projetos <br/>de qualidade para transformar ambientes internos ou externos. <br/>Nós nos esforçamos para criar um cenário com naturalidade para você.</p>
         </div>
     );
