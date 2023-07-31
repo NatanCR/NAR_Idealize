997b048edcc939598496648c3e21ce979a37969f
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-07-31 14:38:36 -0300

    feat: mudando o texto do sobre

--- site-nar-idealize/src/components/SectionAbout/About.js
+++ site-nar-idealize/src/components/SectionAbout/About.js
@@ -8,7 +8,7 @@ function About() {
             <div class="container text-left">
                 <h1>Sobre nós</h1>
                 <div className="row justify-content-left">
-                <p class="text-justify">NAR Idealize, é uma empresa de paisagismo dedicada a criar ambientes naturais e modernos para eventos e empresas. Nossa missão é envolver as pessoas em cenários autênticos e significativos, valorizando a conexão com a natureza. Com nossa paixão pelo meio ambiente, buscamos inspirar a inserção da natureza em todos os espaços e momentos, transformando sonhos em realidade através de projetos excepcionais. Queremos que o paisagismo natural faça parte de tudo o que nos rodeia, valorizando cada detalhe e enaltecendo a beleza da natureza em nossa vida diária.</p>
+                <p class="text-justify">NAR Idealize, é uma empresa de paisagismo dedicada a criar ambientes naturais e modernos para eventos e empresas. Nossa missão é envolver as pessoas em cenários autênticos e significativos, valorizando a conexão com a natureza. Com nossa paixão pelo meio ambiente, buscamos inspirar a inserção da natureza em todos os espaços e momentos, transformando sonhos em realidade através de projetos excepcionais. Trabalhamos com criatividade, agilidade e competência para tornar a sua ideia em realidade, nos comprometemos com o seu projeto buscando a melhor solução paisagística pra você.</p>
                 </div>
             </div>
         </div>
